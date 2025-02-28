de161130493ea5ca8278a0210637f85f5a31a18c
firidaus <fildauskakz@gmaol.com>
2025-02-28 16:48:06 +0300

    port

--- app.py
+++ app.py
@@ -130,5 +130,9 @@ def upload_file():
 def index():
     return render_template('index.html')
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
+
